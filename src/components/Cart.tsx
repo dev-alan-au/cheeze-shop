60a1ef1101f6ec44764a86718125c60c62bc232d
Alan Lai <dev.alan.au@gmail.com>
2023-08-17 13:12:04 +1000

    Add qty input

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -35,11 +35,19 @@ function CartItem({ product, qty }: { product: Product, qty: number }) {
   const removeItem = () => dispatch({ type: 'cart/removeItem', payload: { product } });
   const decrementQty = () => dispatch({ type: 'cart/decrementItem', payload: { product } });
   const incrementQty = () => dispatch({ type: 'cart/incrementItem', payload: { product } });
+  const handleQtyUpdate = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const newQty = Number(ev.target.value);
+    dispatch({ type: 'cart/updateItemQty', payload: { product, qty: newQty } });
+  }
 
   return (
     <div className="flex">
       <div className="flex-1">{name}</div>
-      <div className="flex-1"><button onClick={decrementQty}>-</button>{qty}<button onClick={incrementQty}>+</button></div>
+      <div className="flex-1">
+        <button onClick={decrementQty}>-</button>
+        <input type="number" min={0} step={1} value={qty} onChange={handleQtyUpdate} className="text-center" />
+        <button onClick={incrementQty}>+</button>
+      </div>
       <div className="flex-auto text-right">{formatPrice(price * qty)}</div>
       <div className="flex-auto text-right"><button onClick={removeItem}>Remove</button></div>
     </div>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -27,13 +27,22 @@ function ProductList({ product }: { product: Product }) {
 
   const decrementQty = () => dispatch({ type: 'cart/decrementItem', payload: { product } });
   const incrementQty = () => dispatch({ type: 'cart/incrementItem', payload: { product } });
+  const handleQtyUpdate = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const newQty = Number(ev.target.value);
+    dispatch({ type: 'cart/updateItemQty', payload: { product, qty: newQty } });
+  }
+
   const itemInCart = cart.items.find(item => item.product.id == product.id);
 
   return (
     <div className="flex gap-2 mb-2">
       <div className="flex-1">{name}</div>
       <div className="flex-1">{description}</div>
-      <div className="flex-1"><button onClick={decrementQty}>-</button>{itemInCart?.qty || 0}<button onClick={incrementQty}>+</button></div>
+      <div className="flex-1">
+        <button onClick={decrementQty}>-</button>
+        <input type="number" min={0} step={1} value={itemInCart?.qty || 0} onChange={handleQtyUpdate} className="text-center" />
+        <button onClick={incrementQty}>+</button>
+      </div>
       <div className="flex-1 text-right">{formatPrice(price)}</div>
     </div>
   )
